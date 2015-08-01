5dd30ca0594ef6e45cbf06cba96082ba9658f50e
craftpaperbag <kazu11518@gmail.com>
2015-08-01 22:34:09 +0900

    #2 go

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"github.com/zenazn/goji"
+	"github.com/zenazn/goji/web"
+	"net/http"
+)
+
+func hello(c web.C, w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", c.URLParams["name"])
+}
+
+func main() {
+	goji.Get("/hello/:name", hello)
+	goji.Serve()
+}
