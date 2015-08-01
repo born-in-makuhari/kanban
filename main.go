3fce3d08210c2424ef50622eac9a20eafe090068
craftpaperbag <kazu11518@gmail.com>
2015-08-02 01:09:37 +0900

    #2 confirm heroku action

--- main.go
+++ main.go
@@ -11,7 +11,12 @@ func hello(c web.C, w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %s!", c.URLParams["name"])
 }
 
+func root(c web.C, w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello goji!")
+}
+
 func main() {
 	goji.Get("/hello/:name", hello)
+	goji.Get("/", root)
 	goji.Serve()
 }
