c36fc9cc619d7ac2e0dccbc5571349a14ea8b182
craftpaperbag <kazu11518@gmail.com>
2015-08-02 10:14:16 +0900

    #2 :octocat: thanks to yossi/ace

--- main.go
+++ main.go
@@ -2,17 +2,29 @@ package main
 
 import (
 	"fmt"
+	"github.com/yosssi/ace"
 	"github.com/zenazn/goji"
 	"github.com/zenazn/goji/web"
 	"net/http"
 )
+
+// Ace Template Engine
+// https://github.com/yosssi/ace/blob/master/documentation/getting-started.md
 
 func hello(c web.C, w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %s!", c.URLParams["name"])
 }
 
 func root(c web.C, w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello goji!")
+	tpl, err := ace.Load("index", "", nil)
+	if err != nil {
+		panic(err)
+	}
+	data := map[string]interface{}{"Title": "Kanban"}
+	err = tpl.Execute(w, data)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func Route(m *web.Mux) {
