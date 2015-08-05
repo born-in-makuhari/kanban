28e8692a125d31925c9324ef9f63a776f97edbf1
craftpaperbag <kazu11518@gmail.com>
2015-08-05 23:03:06 +0900

    #8 + kanban/public/{css, js, imgs, fonts}

--- config.rb
+++ config.rb
@@ -4,11 +4,12 @@ require 'compass/import-once/activate'
 
 
 # Set this to the root of your project when deployed:
-http_path = "/public"
-css_dir = "css"
+http_path = "/"
+css_dir = "public/css"
 sass_dir = "sass"
-images_dir = "imgs"
-javascripts_dir = "js"
+images_dir = "public/imgs"
+javascripts_dir = "public/js"
+fonts_dir = "public/fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
